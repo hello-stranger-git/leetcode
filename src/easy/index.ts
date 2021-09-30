1cfa0f773ae9724bfa0df4512037fc25e97fd325
xieyingcai <xieyingcai@91cyt.com>
2021-09-30 15:24:36 +0800

    完成罗马数字转整数

--- src/easy/index.ts
+++ src/easy/index.ts
@@ -20,4 +20,36 @@ class Easy {
         let num = x
         return Number(x.toString().split("").reverse().join("")) === num
     }
+    // 13. 罗马数字转整数
+    romanToInt(s: string): number {
+        //规则
+        const map: any = {
+            I: 1,
+            V: 5,
+            X: 10,
+            L: 50,
+            C: 100,
+            D: 500,
+            M: 1000,
+        }
+        //最后算出来的数
+        let sum: number = 0
+        for (let index = 0; index < s.length; index++) {
+            debugger
+            console.log(s[index])
+            //当前值
+            const current: number = map[s[index]]
+            //下一个值
+            const next: number = map[s[index + 1]]
+
+            if (current < next) {
+                sum -= current
+            } else {
+                sum += current
+            }
+        }
+        return sum
+
+    };
 }
+new Easy().romanToInt("II")
